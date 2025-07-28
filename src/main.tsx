f22fa369980054b04b1c8f48e942a911600668c6
Michael Wright <mkwright@gmail.com>
2025-07-27 23:49:45 -0600

    Alright, have the buffer working, next is pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer'
 import './index.css'
 import App from './App.tsx'
+
+// Make Buffer available globally for gray-matter
+window.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
