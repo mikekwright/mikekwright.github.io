510e0a170bf1ddb450f04fd95721526af41658c5
Michael Wright <mkwright@gmail.com>
2025-07-27 22:06:45 -0600

    Working to fix github flow

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    target: 'es2015',
+    minify: 'terser'
   }
 })
